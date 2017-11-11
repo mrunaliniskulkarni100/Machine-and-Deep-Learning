eb918167349ad0600566c613b94ca7c30005ac5d
Ishan JOshi <ishanjoshi02@gmail.com>
2017-11-11 07:16:28 +0530

    Added Code for Naive Bayes bernoulli Classifier

---
+++ Code/Machine Learning/Naive Bayes Classifier/naive_bayes_classifier_bernoulli.py
@@ -0,0 +1,7 @@
+import numpy as np
+from sklearn.naive_bayes import BernoulliNB
+X = np.random.randint(2, size=(6, 100))
+Y = np.array([1, 2, 3, 4, 4, 5])
+clf = BernoulliNB()
+clf.fit(X, Y)
+print(clf.predict(X[2:3]))
