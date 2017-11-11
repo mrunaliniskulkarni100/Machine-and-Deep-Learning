9f8baf8868f29cf615e2083e636b0b43f9eaeb94
Ishan JOshi <ishanjoshi02@gmail.com>
2017-11-11 07:28:30 +0530

    Added Code for Decision Tree Regressor

---
+++ Code/Machine Learning/Decision Trees/decision_tree_regressor.py
@@ -0,0 +1,19 @@
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_iris
+
+data_set=load_iris()
+
+X_train, X_test, y_train, y_test = train_test_split(data_set.data, data_set.target, random_state=0)
+
+DecReg=DecisionTreeRegressor()
+DecReg.fit(X_train, y_train)
+
+results = DecReg.predict(X_test)
+
+for i in range(len(results)):
+
+	if int(results[i]) == int(y_test[i]):
+		print(True)
+	else:
+		print(False) 
