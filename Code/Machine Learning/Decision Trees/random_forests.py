4ce9f3fc5022510b38f87adf44dbeed834cae171
Ishan JOshi <ishanjoshi02@gmail.com>
2017-11-11 12:27:59 +0530

    Modified Random Forests Code to generate many more trees

--- Code/Machine Learning/Decision Trees/random_forests.py
+++ Code/Machine Learning/Decision Trees/random_forests.py
@@ -1,5 +1,5 @@
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.datasets import make_moons
+from sklearn.datasets import make_moons, load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.tree import export_graphviz
 
@@ -18,3 +18,18 @@ for i in range(len(forest.estimators_)):
 	graph=pydotplus.graph_from_dot_data(export_graphviz(forest.estimators_[i], out_file=None))
 	graph.write_png('random_forest_tree_' + str(i) + '.png')
 
+dataset=load_iris()
+
+X_train, X_test, y_train, y_test = train_test_split(dataset.data, dataset.target)
+
+forest = RandomForestClassifier(n_estimators=100, random_state=2)
+forest.fit(X_train, y_train)
+
+print("Accuracy of Training data: {:.5f}".format(forest.score(X_train, y_train)))
+print("Accuracy of Testing data: {:.5f}".format(forest.score(X_test, y_test)))
+
+import pydotplus
+
+for i in range(len(forest.estimators_)):
+	graph=pydotplus.graph_from_dot_data(export_graphviz(forest.estimators_[i], out_file=None))
+	graph.write_png('random_forest_tree_' + str(i) + '.png')
