a6689f0c5baaeb7e312ac709d33acac4cc24e01a
Ishan JOshi <ishanjoshi02@gmail.com>
2017-11-11 07:48:27 +0530

    Added code for visualising trees

--- Code/Machine Learning/Decision Trees/random_forests.py
+++ Code/Machine Learning/Decision Trees/random_forests.py
@@ -1,6 +1,7 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import make_moons
 from sklearn.model_selection import train_test_split
+from sklearn.tree import export_graphviz
 
 X, y = make_moons(n_samples=100, noise=0.25, random_state=3)
 X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=42)
@@ -10,3 +11,10 @@ forest.fit(X_train, y_train)
 
 print("Accuracy of Training data: {:.5f}".format(forest.score(X_train, y_train)))
 print("Accuracy of Testing data: {:.5f}".format(forest.score(X_test, y_test)))
+
+import pydotplus
+
+for i in range(len(forest.estimators_)):
+	graph=pydotplus.graph_from_dot_data(export_graphviz(forest.estimators_[i], out_file=None))
+	graph.write_png('random_forest_tree_' + str(i) + '.png')
+
