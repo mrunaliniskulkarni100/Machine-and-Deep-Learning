5c0a61958e40de06b059a1593ca993d16df9dc7d
Ishan JOshi <ishanjoshi02@gmail.com>
2017-11-11 07:40:29 +0530

    Modified Code for Decision Tree Regressor

--- Code/Machine Learning/Decision Trees/decision_tree_regressor.py
+++ Code/Machine Learning/Decision Trees/decision_tree_regressor.py
@@ -9,6 +9,9 @@ X_train, X_test, y_train, y_test = train_test_split(data_set.data, data_set.targ
 DecReg=DecisionTreeRegressor()
 DecReg.fit(X_train, y_train)
 
+print("Accuracy of Training data: {:.5f}".format(DecReg.score(X_train, y_train)))
+print("Accuracy of Testing data: {:.5f}".format(DecReg.score(X_test, y_test)))
+
 results = DecReg.predict(X_test)
 
 for i in range(len(results)):
